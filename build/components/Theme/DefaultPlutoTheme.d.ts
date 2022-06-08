bdef76561f260885cc645563c260e3c4c00f05ef
andy_gallagher <andy.gallagher@theguardian.com>
2022-06-08 17:55:35 +0100

    we don't need to commit 'build' any more :D

--- build/components/Theme/DefaultPlutoTheme.d.ts
+++
@@ -1,2 +0,0 @@
-declare const defaultPlutoTheme: (dark: boolean) => import("@material-ui/core/styles").Theme;
-export { defaultPlutoTheme };
--- build/utils/AppLinks.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const hrefIsTheSameDeploymentRootPath: (href: string) => boolean;
-export declare const getDeploymentRootPathLink: (href: string) => string;
